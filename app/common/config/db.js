61bb9e42e6e07e2fa18a8c76134d82f79f1b7c44
millzhang <876753183@qq.com>
2018-01-30 15:16:36 +0800

    'update'

--- app/common/config/db.js
+++ app/common/config/db.js
@@ -1,20 +1,25 @@
 'use strict';
 /**
-               * db config
-               * @type {Object}
-               */exports.__esModule = true;exports.default =
-{
+ * db config
+ * @type {Object}
+ */
+
+exports.__esModule = true;
+exports.default = {
   type: 'mysql',
   log_sql: true,
   log_connect: true,
   adapter: {
     mysql: {
-      host: '127.0.0.1',
+      host: '116.85.44.204',
       port: '3306',
       database: 'liblog',
       user: 'root',
-      password: 'shamrock5488',
+      password: '123456',
       prefix: 'li_',
-      encoding: 'utf8' },
-
-    mongo: {} } };
+      encoding: 'utf8'
+    },
+    mongo: {}
+  }
+};
+//# sourceMappingURL=db.js.map
--- src/common/config/db.js
+++ src/common/config/db.js
@@ -13,7 +13,7 @@ export default {
       port: '3306',
       database: 'liblog',
       user: 'root',
-      password: 'shamrock5488',
+      password: '123456',
       prefix: 'li_',
       encoding: 'utf8'
     },
